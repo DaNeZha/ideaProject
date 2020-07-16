bbe1286b49b177aa0e596b559809c87948337501
wd <1005689490@qq.com>
2020-07-16 09:56:29 +0800

    创建分支

--- hellogit/src/main/java/com/weidong/HelloGit.java
+++ hellogit/src/main/java/com/weidong/HelloGit.java
@@ -5,5 +5,6 @@ public class HelloGit {
         System.out.println("hello git");
         System.out.println("hello git 更新1");
         System.out.println("hello git 更新2");
+        System.out.println("创建分支");
     }
 }
